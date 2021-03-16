5594a17efa97ce08a58d6d749b7c19a0527f8206
Khaue Poerner <khaue.p@grupovvale.local>
2021-03-16 09:11:00 -0300

    Link to youtube videos

--- components/youtube-video.tsx
+++ components/youtube-video.tsx
@@ -3,16 +3,18 @@ import Image from 'next/image'
 const YoutubeVideo = ({ video }) => {
   console.log(video)
   return (
-    <div className="flex flex-row mt-1">
-      <Image
-        src={video.snippet.thumbnails.default.url}
-        width={video.snippet.thumbnails.default.width}
-        height={video.snippet.thumbnails.default.height} />
+    <a target="_blank" href={"https://www.youtube.com/watch?v=" + video.id.videoId }>
+      <div className="flex flex-row mt-1">
+        <Image
+          src={video.snippet.thumbnails.default.url}
+          width={video.snippet.thumbnails.default.width}
+          height={video.snippet.thumbnails.default.height} />
 
-      <p className="pl-2 font-semibold">
-        {video.snippet.title}
-      </p>
-    </div>
+        <p className="pl-2 font-semibold">
+          {video.snippet.title}
+        </p>
+      </div>
+    </a>
   )
 }
 
