ec318371792800b343c72566c291d6ecde3ff3a5
Khaue Poerner <khaue.p@grupovvale.local>
2021-03-16 17:48:18 -0300

    Before package install date-fns

--- components/youtube-video.tsx
+++ components/youtube-video.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 
 const YoutubeVideo = ({ video }) => {
-  console.log(video)
   return (
     <a target="_blank" href={"https://www.youtube.com/watch?v=" + video.id.videoId }>
       <div className="flex flex-row mt-1">
@@ -17,6 +16,8 @@ const YoutubeVideo = ({ video }) => {
         <p className="pl-2 font-semibold">
           {video.snippet.title}
         </p>
+        <span>
+        </span>
       </div>
     </a>
   )
